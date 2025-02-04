93ea798199ca089748e4a57e94bbb82fae9fea58
Edgar <ebyted@gmail.com>
2025-02-03 22:01:48 -0800

    heroku fix

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -4,7 +4,7 @@ import datetime
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-from django.utils.timezone import utc
+from datetime import timezone
 
 
 class Migration(migrations.Migration):
@@ -36,7 +36,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200, verbose_name='Título')),
                 ('content', models.TextField(verbose_name='Contenido')),
-                ('published', models.DateTimeField(default=datetime.datetime(2018, 3, 5, 17, 51, 50, 818442, tzinfo=utc), verbose_name='Fecha de publicación')),
+                ('published', models.DateTimeField(default=datetime.datetime(2018, 3, 5, 17, 51, 50, 818442, tzinfo=timezone.utc), verbose_name='Fecha de publicación')),
                 ('image', models.ImageField(blank=True, null=True, upload_to='blog', verbose_name='Imagen')),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')),
                 ('updated', models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')),
